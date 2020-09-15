05e7a8788110a9adff73f484c01f7b30420d61d5
chsprithvi <chsprithvi@gmail.com>
2020-09-15 15:22:05 +0530

    [Siddhartha]Added:While loop to limit 50% of stake to win or loose

--- Gambling.py
+++ Gambling.py
@@ -1,20 +1,27 @@
 """
-A player with stake $100 and who bets $1 for each game
-Wins $1 if won and looses $1 is lost
+A player with stake $100 and who bets $1 for each game, resigns if won/lost 50% of stake
+
 """
 import random 
 stake=100
 bet=1
 print("The player stake value:",stake)
 print("The player bet value for the game :",bet)
+percentLimit=0.5
+minStake=stake-(stake*percentLimit)
+maxStake=stake+(stake*percentLimit)
 
-"""
-Winning=0
-Loosing=1
-"""
-if random.randint(0,1) == 1:
-    print("~~~~~~You Lost~~~~~~")
-    print("The stake value after Loosing:",stake-bet)
-else:
-    print("~~~~~~You Won~~~~~~")
-    print("The stake value after Winning:",stake+bet)
+while stake > minStake and stake < maxStake:
+    """
+    Winning=0
+    Loosing=1
+    """
+    if random.randint(0,1) == 1:
+        print("~~~~~~You Lost~~~~~~")
+        print("The stake value after Loosing:",stake-bet)
+        stake=stake-bet
+    else:
+        print("~~~~~~You Won~~~~~~")
+        print("The stake value after Winning:",stake+bet)
+        stake=stake+bet
+print("Sorry, Your stake value is limited to",stake)

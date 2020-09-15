80812690523c1002c0cde380ad64e87d0368307f
chsprithvi <chsprithvi@gmail.com>
2020-09-15 15:31:20 +0530

    [Siddhartha]Added:function to continue playing iff profit for month and stop if loss

--- Gambling.py
+++ Gambling.py
@@ -3,52 +3,92 @@ A player with stake $100 and who bets $1 for each game.
 Displaying the total Winning and Loosing days for each month.
 Considering profit or loss for a month
 """
+
 import random 
-stake=100
-bet=1
-print("The player stake value:",stake)
-print("The player bet value for the game :",bet)
 
-percentLimit=0.5
-minStake=stake-(stake*percentLimit)
-maxStake=stake+(stake*percentLimit)
+"""
+calculateMonth function helps in calculating daily profit and loss, returns difference b/w total lost and profit
+"""
 
-days=30
-day=1
-daysWon=0
-daysLost=0
-storeDaysWon={}
-storeDaysLost={}
+def calculateMonth():
+    stake=100
+    bet=1
+    days=30
+    day=1
+    totalLost=0
+    totalWon=0
+    daysWon=0
+    daysLost=0
 
-while day <= days:
+    print("The player stake value:",stake)
+    print("The player bet value for the game :",bet)
     
-    amount=stake
-    wonPerDay=0
-    lostPerDay=0
+    percentLimit=0.5
+    minStake=stake-(stake*percentLimit)
+    maxStake=stake+(stake*percentLimit)
+
+
+    while day <= days:
+        
+        amount=stake
+        wonPerDay=0
+        lostPerDay=0
     
-    while amount > minStake and amount < maxStake:
-        """
-        Winning=0
-        Loosing=1
-        """
-        if random.randint(0,1) == 1:
-            amount=amount-bet
-            lostPerDay=lostPerDay+1
+        while amount > minStake and amount < maxStake:
+            """
+            Winning=0
+            Loosing=1
+            """
+            if random.randint(0,1) == 1:
+                amount=amount-bet
+                lostPerDay=lostPerDay+1
+            else:
+                amount=amount+bet
+                wonPerDay=wonPerDay+1
+        if wonPerDay > lostPerDay:
+            daysWon=daysWon+1 
+        else: 
+            daysLost=daysLost+1
+    
+        totalLost=totalLost+lostPerDay
+        totalWon=totalWon+wonPerDay
+        day=day+1
+    print("Number of days won",daysWon, " and Number of days lost",daysLost,"in 30 days")
+    diffAmount = totalWon - totalLost
+    return(diffAmount)
+
+"""
+checkMonth fuction recurcively helps to check if profit or loss per month
+"""
+
+def checkMonth():
+    checkProfitLoss=calculateMonth()
+
+    if checkProfitLoss>0:
+        print("profit for overall month:",checkProfitLoss)
+        print("congrats! You can play for next month")
+        
+        flag=str(input("Press 'Y' to Continue or any other key to exit"))
+    
+        if flag=='y'or'Y':
+            checkMonth()
         else:
-            amount=amount+bet
-            wonPerDay=wonPerDay+1
-    storeDaysWon[day]=wonPerDay
-    storeDaysLost[day]=lostPerDay
+            exit()
+    elif checkProfitLoss<0:
+        print("loss for overall month:",abs(checkProfitLoss))
+        print("Sorry, You cannot continue next month. Better luck Next time :-) ")
+    else:
+        print("neither loss nor profit")
+        print("Sorry, You cannot continue next month. Better luck Next time :-) ")
 
-    day=day+1
+"""
+****************
+Main method calls the demo fuction to check if month has profit or loss
+****************
+"""
 
-
-print(storeDaysLost)
-print(storeDaysWon)
-luckyDay=max(storeDaysWon, key=storeDaysWon.get)
-unLuckyDay=max(storeDaysLost, key=storeDaysLost.get)
-
-print("The luckiest day in the month is",luckyDay,"with profit",storeDaysWon[luckyDay])
-print("The Un-luckiest day in the month is",unLuckyDay,"with loss",storeDaysLost[unLuckyDay])
-
-
+def main():
+    print("Welcome to the World of Gambling")
+    checkMonth()
+if __name__ == "__main__":
+    main()
